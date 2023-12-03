a47edfeb1b8fbbf93eac5bb45b53f2f0e6801ef4
DevinGlaw <125889247+pandoradevin@users.noreply.github.com>
2023-12-03 19:53:26 +0800

    Update auth.ts

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,4 +1,4 @@
-export const officialAPIEndpoint = 'https://deviner.link/devinerapi1/v1/chat/completions';
+export const officialAPIEndpoint = 'https://ningmengguorou.top:94/v1/chat/completions';
 const customAPIEndpoint =
   import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://api.openai.com/v1/chat/completions';
 export const defaultAPIEndpoint =
