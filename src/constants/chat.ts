5b0d2452fb0c50c0747eb0a67d3fc0510846ff32
DevinGlaw <125889247+pandoradevin@users.noreply.github.com>
2023-12-26 15:23:06 +0800

    Update chat.ts

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -29,7 +29,7 @@ export const modelOptions: ModelOptions[] = [
   // 'gpt-4-32k-0314',
 ];
 
-export const defaultModel = 'gpt-3.5-turbo';
+export const defaultModel = 'gpt-4';
 
 export const modelMaxToken = {
   'gpt-3.5-turbo': 4096,
@@ -102,7 +102,7 @@ export const modelCost = {
   },
 };
 
-export const defaultUserMaxToken = 4000;
+export const defaultUserMaxToken = 8192;
 
 export const _defaultChatConfig: ConfigInterface = {
   model: defaultModel,
