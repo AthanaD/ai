8fa82e5dded91b1b943422f4355644557aba90e7
Athena <125889247+Athanad@users.noreply.github.com>
2023-12-31 01:37:10 +0800

    Update Me.tsx

--- src/components/Menu/MenuOptions/Me.tsx
+++ src/components/Menu/MenuOptions/Me.tsx
@@ -8,7 +8,7 @@ const Me = () => {
   return (
     <a
       className='flex py-2 px-2 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm'
-      href='https://github.com/Athana-devin/devinchatgpt'
+      href='https://github.com/Athanad/devinchatgpt'
       target='_blank'
     >
       <HeartIcon />
