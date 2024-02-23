2a1f5598c69b65e4dac86cc29e8068a0a65211d1
Athena <125889247+AthanaD@users.noreply.github.com>
2024-02-24 00:10:56 +0800

    Update chat.ts

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -21,17 +21,17 @@ export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo',
   'gpt-3.5-turbo-16k',
   'gpt-3.5-turbo-1106',
+  'gpt-3.5-turbo-0125',
   'gpt-4',
   'gpt-4-32k',
-  'gpt-4-mobile',
-  'gpt-4-1106-preview'
+  'gpt-4-1106-preview',
   'gpt-4-0125-preview'
   // 'gpt-3.5-turbo-0301',
   // 'gpt-4-0314',
   // 'gpt-4-32k-0314',
 ];
 
-export const defaultModel = 'gpt-4';
+export const defaultModel = 'gpt-3.5-turbo';
 
 export const modelMaxToken = {
   'gpt-3.5-turbo': 4096,
@@ -40,10 +40,10 @@ export const modelMaxToken = {
   'gpt-3.5-turbo-16k': 16384,
   'gpt-3.5-turbo-16k-0613': 16384,
   'gpt-3.5-turbo-1106': 16384,
+  'gpt-3.5-turbo-0125': 16384,
   'gpt-4': 8192,
   'gpt-4-0314': 8192,
   'gpt-4-0613': 8192,
-  'gpt-4-mobile': 8192,
   'gpt-4-32k': 32768,
   'gpt-4-32k-0314': 32768,
   'gpt-4-32k-0613': 32768,
@@ -76,6 +76,10 @@ export const modelCost = {
     prompt: { price: 0.001, unit: 1000 },
     completion: { price: 0.0015, unit: 1000 },
   },
+  'gpt-3.5-turbo-0125': {
+    prompt: { price: 0.0005, unit: 1000 },
+    completion: { price: 0.0015, unit: 1000 },
+  },
   'gpt-4': {
     prompt: { price: 0.03, unit: 1000 },
     completion: { price: 0.06, unit: 1000 },
@@ -85,10 +89,6 @@ export const modelCost = {
     completion: { price: 0.06, unit: 1000 },
   },
   'gpt-4-0613': {
-    prompt: { price: 0.03, unit: 1000 },
-    completion: { price: 0.06, unit: 1000 },
-  },
-  'gpt-4-mobile': {
     prompt: { price: 0.03, unit: 1000 },
     completion: { price: 0.06, unit: 1000 },
   },
@@ -108,9 +108,13 @@ export const modelCost = {
     prompt: { price: 0.01, unit: 1000 },
     completion: { price: 0.03, unit: 1000 },
   },
+  'gpt-4-0125-preview': {
+    prompt: { price: 0.01, unit: 1000 },
+    completion: { price: 0.03, unit: 1000 },
+  },
 };
 
-export const defaultUserMaxToken = 8192;
+export const defaultUserMaxToken = 4000;
 
 export const _defaultChatConfig: ConfigInterface = {
   model: defaultModel,
