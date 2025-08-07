ac468cceb05dbd5a10c1800a4e1f369e6d138f9f
Athena <125889247+AthanaD@users.noreply.github.com>
2025-08-07 22:37:26 +0800

    Update chat.ts

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -61,7 +61,10 @@ export type ModelOptions =
   | 'gpt-3.5-turbo'
   | 'gpt-3.5-turbo-16k'
   | 'gpt-3.5-turbo-1106'
-  | 'gpt-3.5-turbo-0125';
+  | 'gpt-3.5-turbo-0125'
+  | 'gpt-4.1' // <-- 新增
+  | 'gpt-4.1-mini' // <-- 新增
+  | 'gpt-4.1-nano';
 // | 'gpt-3.5-turbo-0301';
 // | 'gpt-4-0314'
 // | 'gpt-4-32k-0314'
